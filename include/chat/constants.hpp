63041d07a27b25b90ba7c231201295d3f9be5300
turchyack <turchyack@gmail.com>
2023-08-11 01:24:09 +0300

    добавлен забытый файл

---
+++ include/chat/constants.hpp
@@ -0,0 +1,8 @@
+#if !defined(__CHAT_CONSTANTS_HPP_INCLUDED)
+#define __CHAT_CONSTANTS_HPP_INCLUDED
+
+unsigned SERVER_ADDRESS = 0x7f000001;
+const unsigned DATA_SIZE_MAX = 64;
+const unsigned SERVER_PORT = 10001;
+
+#endif
