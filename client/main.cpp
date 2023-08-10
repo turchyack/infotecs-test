6a0c07e412a30eb375cbd2ef1a0eac69bc8ffe86
turchyack <turchyack@gmail.com>
2023-08-10 23:40:17 +0300

    добавлен счёт суммы

--- client/main.cpp
+++ client/main.cpp
@@ -5,6 +5,10 @@
 #include <algorithm>
 #include <thread>
 #include <unistd.h>
+#include <mutex>
+#include <condition_variable>
+
+const size_t DATA_SIZE_MAX = 64;
 
 
 // return
@@ -16,7 +20,7 @@
 // >0 - принята строка цифр, значение представляет длину строки
 //
 ssize_t input_digits_line(FILE* stream, char* buffer, size_t buffer_size) {
-    printf("введите строку: ");
+    printf("input: ");
     fflush(stdout);
     int c = 0;
     size_t line_length = 0;
@@ -82,7 +86,7 @@ ssize_t subtitude_even_digits(char* result_buffer,  size_t result_buffer_size, s
 // line_length - длина строки
 // -1 - ошибка. заданый буфер имеет недостаточный размер
 //
-ssize_t transform_line(const char* line, size_t line_length, char* result_buffer, size_t result_buffer_size) {
+ssize_t transform_data(const char* line, size_t line_length, char* result_buffer, size_t result_buffer_size) {
     if(result_buffer_size < line_length) {
         return -1;
     }
@@ -92,28 +96,35 @@ ssize_t transform_line(const char* line, size_t line_length, char* result_buffer
     return subtitude_even_digits(result_buffer,  result_buffer_size, line_length);
 }
 
-void transfer_line_to_sender(char* result_buffer, size_t result_buffer_size) {
-    printf("переданная строка[%u]: '%.*s'\n",
-            static_cast<unsigned>(result_buffer_size),
-            static_cast<unsigned>(result_buffer_size),
-            result_buffer
-        );
-
+unsigned calc_sum(char* data, size_t data_length) {
+    unsigned sum = 0;
+    for(size_t i = 0; i < data_length; ++i) {
+        if(isdigit(data[i])) {
+            sum += data[i] -'0';
+        }
+    }
+    return sum;
 }
 
 class Sender {
 public:
     Sender();
-    void stop();
     ~Sender();
-
+    void stop();
+    void signal();
+    void send(char* buffer, size_t buffer_size);
 private:
-    bool sender_work;
-    std::thread thread;
     static void main_wrapper(Sender* self) {
         self->main();
     }
     void main();
+    static const size_t buffer_size = DATA_SIZE_MAX*2;
+    char shared_buffer[buffer_size] = {0};
+    size_t shared_data_length = 0;
+    bool sender_work;
+    std::thread thread;
+    std::mutex m;
+    std::condition_variable cv;
 };
 
 Sender::Sender() :  sender_work(true), thread(main_wrapper, this) {
@@ -126,27 +137,70 @@ Sender::~Sender() {
 }
 
 void Sender::main() {
-    fprintf(stderr,"нить запущена\n");
-    unsigned x = 0;
+    char send_data[buffer_size] = {};
+    size_t length = 0;
+    std::unique_lock lk(m);
+
     while(sender_work) {
-        fprintf(stderr, "sleep%u\n",x);
-        sleep(3);
-        x++;
+       // fprintf(stderr, "debug wait for cv\n");
+        cv.wait(lk);
+        if(shared_data_length !=0) {
+            memcpy(send_data, shared_buffer, shared_data_length);
+            length = shared_data_length;
+            memset(shared_buffer, 0, shared_data_length*sizeof(shared_buffer[0]));
+            shared_data_length = 0;
+        }
+        else {
+            continue;
+        }
+
+        lk.unlock();
+        fprintf(stderr, "do long processing\n");
+        {
+            fprintf(stderr, "sender input data: '%.*s'\n", static_cast<unsigned>(length), send_data);
+            unsigned sum = calc_sum(send_data, length);   // тут ставим соединение
+            fprintf(stderr, "data sum = %u\n", sum);
+
+
+            sleep(1);
+
+        }
+
+        lk.lock();
     }
-    fprintf(stderr,"конец нити\n");
+
+
+    fprintf(stderr,"debug end of thread\n");
 }
 
 void Sender::stop() {
     fprintf(stderr,"stop\n");
     sender_work = false;
+    cv.notify_one();
     thread.join();
 }
 
+void Sender::send(char* data, size_t data_length) {
+
+    if(buffer_size < data_length) {
+        fprintf(stderr,"buffer too small\n");
+    }
+    std::unique_lock lk(m);
+
+    memcpy(shared_buffer, data, data_length);
+    this->shared_data_length = data_length;
+    lk.unlock();
+    cv.notify_one();
+
+}
+
+
 int main() {
+
     Sender sender;
 
     // обработка данных потоком №1
-    const size_t buffer_size = 64;
+    const size_t buffer_size = DATA_SIZE_MAX;
     char buffer[buffer_size] = {};
     const size_t result_buffer_size = buffer_size * 2;
     char result_buffer[result_buffer_size] = {};
@@ -171,14 +225,12 @@ int main() {
             }
 
         }
-        printf("Debug: принята строка[%u]: '%.*s'\n", static_cast<unsigned>(line_length), static_cast<unsigned>(line_length), buffer);
-        ssize_t transformed_length = transform_line(buffer,  line_length, result_buffer, result_buffer_size);
+
+        ssize_t transformed_length = transform_data(buffer,  line_length, result_buffer, result_buffer_size);
         if(transformed_length == -1) {
             return -1;
         }
-
-        transfer_line_to_sender(result_buffer, transformed_length);
-
+        sender.send(result_buffer, transformed_length);
     }
     fprintf(stderr, "debug: wait for join\n");
     sender.stop();
